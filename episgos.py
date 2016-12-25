b38e39e10a4ef887400a12fa309049959d6ced10
Michael Vezie <mlv@mit.edu>
2016-12-25 15:36:34 -0500

    Coded epis.movable. Passes unit test. Added Notes. Long tome used to organize my thoughts (WARNING: possibly painful reading)

--- episgos.py
+++ episgos.py
@@ -13,12 +13,23 @@ class movable(ocal.ocal):
     def pascha_offset(self):
         """return tuple, offset in days between self and pascha
         and relevant year"""
+        
+        for year in (self.year, self.year-1):
+            p = ocal.pascha(year)
+            offset = self - p
+            if offset >= -70:
+                break
+
         return offset, year
 
     def getreading(self, wk):
         """Given a week (dict containing keys 0..6).
         returns dict[self.dow]"""
-        pass
+
+        try:
+            return wk[self.dow]
+        except KeyError:
+            return None
 
     def post_theophany(self):
         """handle the Sunday Epistle and Gospel readings
@@ -26,5 +37,9 @@ class movable(ocal.ocal):
         Returns -1 if Sunday after Theophany is Publican/Pharisee
         Returns 0 if there's just Sunday after Theophany before P/P
         Returns 1..4 (N sundays between SunAftTheo and P/P)"""
-        pass
 
+        p = ocal.pascha(self.year)
+        theo = ocal.julian(year=self.year, month=1, day=6)
+        dif = p-theo
+
+        return (dif - 78) / 7
